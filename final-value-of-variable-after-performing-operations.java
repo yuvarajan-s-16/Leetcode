24fbc526ccfb06561d8b8bfb3f481bf0786e76ae
Yuvarajan S <yuvarajanyuvarajan16@gmail.com>
2025-05-12 00:00:11 +0530

    Add files via upload

---
+++ final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+        for (String op : operations) {
+            if (op.contains("+")) {
+                X++;
+            } else {
+                X--;
+            }
+        }
+        return X;
+    }
+}
