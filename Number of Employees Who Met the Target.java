b69e3f05fd1c7d1e6b49b310470f57baf9e4b833
Yuvarajan S <yuvarajanyuvarajan16@gmail.com>
2025-06-21 21:39:48 +0530

    Create Number of Employees Who Met the Target.java

---
+++ Number of Employees Who Met the Target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int c=0;
+        for(int i=0;i<hours.length;i++)
+        {
+            if(hours[i]>=target)
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+}
