a46622362c6f1acf46c2c1eb9c9cbcb8afb17499
Yuvarajan S <yuvarajanyuvarajan16@gmail.com>
2025-07-04 23:27:57 +0530

    Create Count the Number of Consistent Strings.java

---
+++ Count the Number of Consistent Strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int allowedMask = maskString(allowed);
+        int consistStrCnt = 0;
+        for(var word : words){
+            int wordMask = maskString(word);
+            if((wordMask | allowedMask) == allowedMask)consistStrCnt++; 
+        }
+        return consistStrCnt;
+    }
+    private int maskString(String str){
+        int strMask = 0;
+        for(char ch : str.toCharArray()){
+            int relPos = (ch - 'a');
+            strMask |= (1<<relPos);
+        }
+        return strMask;
+    }
+}
